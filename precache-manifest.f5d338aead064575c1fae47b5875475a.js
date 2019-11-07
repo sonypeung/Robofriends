3d30f73392ace07825e59c8a758d90e677270e11
Sony Peung <speung@sii.pl>
2019-11-07 14:13:32 +0100

    Updates

---
+++ precache-manifest.f5d338aead064575c1fae47b5875475a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f48ffde085a409319f2bb23615da1910",
+    "url": "/Robofriends/index.html"
+  },
+  {
+    "revision": "9f52f0c4861e117477fc",
+    "url": "/Robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "3b0ab15691fd4697e50c",
+    "url": "/Robofriends/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "9f52f0c4861e117477fc",
+    "url": "/Robofriends/static/js/2.a09a1be0.chunk.js"
+  },
+  {
+    "revision": "3b0ab15691fd4697e50c",
+    "url": "/Robofriends/static/js/main.0063de6d.chunk.js"
+  },
+  {
+    "revision": "78cab9b9619ddcd920c7",
+    "url": "/Robofriends/static/js/runtime-main.ff6ebadc.js"
+  }
+]);
